d89c399d90865eab8dcee3853fb31959591d77ec
Le Duy Anh <anh160704557@st.utc.edu.vn>
2021-01-29 15:51:46 +0700

    hihi

---
+++ IoC-Demostration/IoC-Demostration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IoC_Demostration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
